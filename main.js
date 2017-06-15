e5e63c2dbcda0aa364b6f72906c1112832991a35
Maxwell Stephens <stephensm2011@gmail.com>
2017-06-15 14:09:52 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+var pos = 0, 
+test, 
+test_status, 
+question, 
+choice, 
+choices, 
+chA, 
+chB, 
+chC, 
+correct = 0;
+
+var questions = [
+    [ "What is 10 + 4?", "12", "14", "16", "B" ],
+    [ "What is 10 + 3?", "13", "14", "16", "A" ],
+    [ "What is 10 + 2?", "14", "16", "12", "C" ],
+    [ "What is 10 + 1?", "11", "14", "16", "A" ]
+];
+
+function _(x) {
+    return document.getElementById(x);
+}
+function renderQuestion() {
+    test = _("test");
+    if(pos >= questions.length) {
+        test.innerHTML = "<h1>"+(correct/questions.length)*100+"%</h1>";
+        test.innerHTML += "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+        _("test_status").innerHTML = "Test Completed";
+        pos = 0;
+        correct = 0;
+        return false;
+    }
+    _("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    question = questions[pos][0];
+    chA = questions [pos][1];
+    chB = questions [pos][2];
+    chC = questions [pos][3];
+    test.innerHTML = "<h3>"+question+"</h3>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++) {
+        if(choices[i].checked) {
+            choice = choices[i].value;
+        }
+    }
+    if(choice == questions[pos][4]) {
+        correct++;
+    }
+    pos++;
+    renderQuestion();
+}
+window.addEventListener("load", renderQuestion, false);
+
